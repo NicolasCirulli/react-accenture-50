1f99f47a159d5e650fc1c8ce702c20f36d323e80
NicolasCirulli <nicolascirulli2@gmail.com>
2024-03-06 11:26:01 -0300

    Saque el hero del layout

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,15 @@
 import LayoutMain from "./LayoutMain";
 
 import Carousel from "../components/Carousel/Carousel";
+import Hero from "../components/Hero";
 const Home = () => {
   return (
-    <LayoutMain imagen="/1.jpg">
+    <LayoutMain>
+      <Hero titulo="League of blogs" imagen="/1.jpg">
+        <h3 className="bg-white text-black font-bold text-2xl text-center p-5">
+          acá tendríamos que poner una descripción
+        </h3>
+      </Hero>
       <main className="grow flex flex-col gap-5 justify-center items-center p-5">
         <Carousel start={50} end={70} />
       </main>
