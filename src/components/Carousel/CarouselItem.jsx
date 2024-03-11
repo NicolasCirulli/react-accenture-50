a9f9afe1ac1b5d10585cfbfa03dfe5427ff40ac5
NicolasCirulli <nicolascirulli2@gmail.com>
2024-03-11 10:34:11 -0300

    fix: estilos

--- src/components/Carousel/CarouselItem.jsx
+++ src/components/Carousel/CarouselItem.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 const CarouselItem = ({ champion }) => {
   return (
-    <article className="w-9/12">
+    <article className="w-9/12 max-w-[500px]">
       <img
-        className="w-full h-80 object-cover object-top"
+        className="w-full h-[350px] object-cover object-top"
         src={champion.image}
         alt={"Imagen de " + champion.name}
       />
       <footer className="bg-white text-black ps-1 w-full">
-        <h2 className="text-3xl font-bold">{champion.name}</h2>
-        <p className="text-2xl italic">{champion.title}</p>
+        <h2 className="text-2xl font-bold">{champion.name}</h2>
+        <p className="text-xl italic">{champion.title}</p>
         <Link
           to={"/personajes/" + champion.id}
           className="text-blue-950 text-lg underline"
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -18,9 +18,12 @@ const Home = () => {
           Explora los campeones!
         </Link>
       </Hero>
-      <main className="min-h-screen grow flex flex-col gap-5 justify-start items-center pt-20">
-        <h2 className="text-3xl font-semibold">Campeones populares</h2>
-        <Carousel start={50} end={70} />
+      <main className="grow flex flex-col items-center">
+        <section className="min-h-screen w-full flex flex-wrap gap-5 justify-center py-5">
+          <Campeones />
+          <Carousel start={50} end={70} />
+        </section>
+        <Builds />
       </main>
     </>
   );
@@ -28,3 +31,60 @@ const Home = () => {
 
 export default Home;
 /* CTRL ALT A atajo de extension de tailwind */
+
+const Campeones = () => {
+  return (
+    <section className="flex flex-wrap w-full justify-center gap-5 px-5">
+      <h2 className="text-5xl font-bold text-center w-full">Campeones</h2>
+      <p className="w-[55ch] text-lg font-semibold">
+        En League of Legends, un campeón es un personaje jugable que se controla
+        en la Grieta del Invocador. Cada campeón tiene un conjunto único de
+        habilidades y estilo de juego que lo distingue de los demás. Los
+        jugadores pueden elegir entre más de 150 campeones, cada uno con su
+        propia historia, apariencia y habilidades.
+      </p>
+      <p className="w-[55ch] text-lg font-semibold">
+        Los campeones se clasifican en diferentes roles, como asesino, tanque,
+        mago, tirador y apoyo. Cada rol tiene un propósito específico en el
+        equipo, y los jugadores deben trabajar juntos para lograr la victoria.
+        Los campeones pueden subir de nivel durante una partida, lo que les
+        permite aumentar sus estadísticas y desbloquear nuevas habilidades.
+      </p>
+      <p className="w-[55ch] text-lg font-semibold">
+        Para dominar un campeón, los jugadores deben aprender sus habilidades,
+        comprender sus fortalezas y debilidades, y practicar su uso en
+        diferentes situaciones. Con el tiempo y la experiencia, los jugadores
+        pueden convertirse en expertos en un solo campeón o aprender a jugar con
+        varios campeones diferentes.
+      </p>
+    </section>
+  );
+};
+
+const Builds = () => {
+  return (
+    <div className="w-full grow p-5 flex flex-wrap gap-8 justify-center bg-zinc-200 text-black">
+      <h2 className="text-4xl font-bold text-center w-full">Builds</h2>
+      <p className="text-lg font-semibold max-w-[55ch]">
+        ¿Quieres convertirte en una fuerza imparable en League of Legends? La
+        sección de builds de nuestra aplicación web te ofrece la clave para el
+        éxito. Aquí encontrarás una completa colección de builds para cada
+        campeón, diseñadas por jugadores profesionales y expertos en LoL.
+      </p>
+      <p className="text-lg font-semibold max-w-[55ch]">
+        Explora una amplia variedad de builds personalizadas para cada rol y
+        estilo de juego. Tanto si eres un jungler agresivo, un midlaner
+        estratégico o un support protector, tenemos la build perfecta para
+        ayudarte a sacar el máximo potencial de tu campeón favorito.
+      </p>
+      <div className="w-full flex justify-center items-center">
+        <Link
+          to="/personajes"
+          className="font-semibold bg-zinc-700 text-white py-2 px-5 rounded self-center"
+        >
+          Explora las builds!
+        </Link>
+      </div>
+    </div>
+  );
+};
